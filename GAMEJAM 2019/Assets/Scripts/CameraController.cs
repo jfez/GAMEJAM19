145cb3e24594f7d967d053280b9ffc26202b768d
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-10-19 00:35:33 +0200

    Player Movement

---
+++ GAMEJAM 2019/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    public Vector3 offset2;
+
+    public Transform pivot;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        transform.position = target.position;
+        //Vector3 offset2 = new Vector3(0.05f, 18.1f, -22);
+        transform.position -= offset2;
+        offset = transform.position - target.position;
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime); //lerp es como una transición entre la posición de 2 vectores en un tiempo x
+
+    }
+}
---
+++ GAMEJAM 2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public Transform pivot;
+    public float rotateSpeed;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        else{
+            moveDirection.x = Input.GetAxis("Horizontal");
+            moveDirection.z = Input.GetAxis("Vertical");
+            moveDirection.x *= speed/2;
+            moveDirection.z *= speed/2;
+        }
+        
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        //Rotation of the player
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(-moveDirection.x, 0f, -moveDirection.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotateSpeed*Time.deltaTime);
+        }
+    }
+}
